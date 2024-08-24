e56a3c4a7997b41792399d1bb8e184160a43470d
eliWeise <eli@weises.org>
2024-08-24 14:29:12 -0700

    Add home page and styling

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-require("dotevn").config();
+require("dotenv").config();
+const path = require("node:path");
 
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-const PORT = 3000;
+const assestsPath = path.join(__dirname, "public");
+app.use(express.static(assestsPath));
+
+
+app.get("/", (req, res) => {
+  res.render("index")
+});
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`My first Express app - listening on port ${PORT}!`));
